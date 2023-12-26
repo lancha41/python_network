bac6ea18ce627f814e7a7fd7587d1196f9574e86
lancha41 <lancha41@gmail.com>
2023-12-26 18:01:40 +0000

    task 4-1

--- exercises/04_data_structures/test_task_4_1.py
+++ exercises/04_data_structures/test_task_4_1.py
@@ -9,7 +9,7 @@ check_pytest(__loader__, __file__)
 
 def test_task_stdout(capsys):
     """
-    Проверка работы задания
+    testing
     """
     import task_4_1
 
@@ -19,7 +19,7 @@ def test_task_stdout(capsys):
     )
     assert (
         out
-    ), "Ничего не выведено на стандартный поток вывода. Надо не только получить нужный результат, но и вывести его на стандартный поток вывода с помощью print"
+    ), "please use print"
     assert (
         correct_stdout == out.strip()
-    ), "На стандартный поток вывода выводится неправильная строка"
+    ), "icorrect"

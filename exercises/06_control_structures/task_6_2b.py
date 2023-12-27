e398f8be300726f8532deea799b641a59004789d
lancha41 <lancha41@gmail.com>
2023-12-27 20:16:53 +0000

    notes

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,36 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip_add=input('please insert ip address (example - 10.1.1.1): ')
+ip_add_list=ip_add.split('.')
+n=ip_add.split('.')
+a = True
+
+while True:
+	try:
+		int(ip_add.replace('.','')) 
+		if len(n)!=4:
+			raise ValueError
+			break
+		elif not(int(n[0])>=0 and int(n[0])<=255 and int(n[1])>=0 and int(n[1])<=255 and int(n[2])>=0 and int(n[2])<=255 and int(n[3])>=0 and int(n[3])<=255):
+			raise ValueError
+			break
+	except ValueError:
+		print("Неправильный IP-адрес")
+		a=False
+		break while a :
+        if int(ip_add_list[0]) >=1 and int(ip_add_list[0]) <=223:
+                print('unicast')
+                break
+        elif int(ip_add_list[0]) >=224 and int(ip_add_list[0]) <=239:
+                print('multicast')
+                break
+        elif int(ip_add_list[0]) == int(ip_add_list[1]) == int(ip_add_list[2]) == int(ip_add_list[3]) == 255:
+                print('local broadcast')
+                break
+        elif int(ip_add_list[0]) == int(ip_add_list[1]) == int(ip_add_list[2]) == int(ip_add_list[3]) == 0:
+                print('unassigned')
+                break
+        else:
+                print('unused')
+                break

9a40e94c780df5cf4a5506627eaa7e31179a4339
lancha41 <lancha41@gmail.com>
2024-01-08 14:53:14 +0000

    9-3

--- exercises/09_functions/task_9_3a.py
+++ exercises/09_functions/task_9_3a.py
@@ -25,3 +25,25 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+def get_int_vlan_map(config_filename):
+    with open(config_filename,'r') as f:
+        file = f.readlines()
+    clear_list=[]
+    access_dict={}
+    trunk_dict={}
+    trunk_vlan=[]
+    for i in file:
+        if 'FastEthernet' in i:
+            clear_list.append(i[10:-1])
+            clear_list.append(file[int(file.index(i)+2)])
+    for i in clear_list:
+        if 'access vlan' in i:
+            access_dict[clear_list[int(clear_list.index(i)-1)]]=int(i.split()[-1])
+            clear_list.remove(i)
+        elif 'trunk allowed' in i:
+            trunk_dict[clear_list[int(clear_list.index(i)-1)]]=trunk_vlan=[int(a) for a in i.split()[4].split(',')]
+        elif 'duplex' in i:
+            access_dict[clear_list[int(clear_list.index(i)-1)]]=int(1)
+            clear_list.remove(i)
+    return access_dict,trunk_dict
+get_int_vlan_map('config_sw1.txt')
